0d626ba3ae2fe5985994e61c18e775187a44a003
Muskra <76000573+Muskra@users.noreply.github.com>
2024-04-28 17:06:34 +0200

    fix Reader issue
    There is a problem with the set.Fields assignation, it seem's that the every pointers returns to nil values

--- lexml.go
+++ lexml.go
@@ -1,171 +1,172 @@
 package lexml
 
 import (
+	"bytes"
+	"encoding/xml"
 	"fmt"
-    "io"
-    "encoding/xml"
+	"io"
 )
 
 // Set is the entrypoint struct to interact with this program with Fields as a list of Tag types, Content that represent the whole XML file, Raw wich represent the raw byte data of the file
 type Set struct {
-    Fields []Tag
-    Content Data
-    Raw io.Reader
+	Fields  []Tag
+	Content Data
+	Raw     []byte
 }
 
 // Data is a recursive representation of a parsed XML file content
 type Data struct {
-    Type *Tag
-    Index int
-    Value string
-    Inners []Data
+	Type   *Tag
+	Index  int
+	Value  string
+	Inners []Data
 }
 
 // Tags represent fields of a XML file, those are generated on the go
 type Tag struct {
-    Id int
-    Name string
+	Id   int
+	Name string
 }
 
 // Parse convert the whole file into a DataSet datastructure
 func (set Set) Parse() ([]Tag, Data, error) {
 
-    decoder := xml.NewDecoder(set.Raw)
+	reader := bytes.NewReader(set.Raw)
+	decoder := xml.NewDecoder(reader)
 
-    tagList := findTags(decoder)
-   
-    data, err := recurse(decoder, tagList)
-    if err != nil {
-        return []Tag{}, Data{}, fmt.Errorf("Parse() -> %s", err)
-    }
+	tagList := findTags(decoder)
 
-    return tagList, data, nil
+	reader = bytes.NewReader(set.Raw)
+	decoder = xml.NewDecoder(reader)
+
+	data, err := recurse(decoder, tagList)
+	if err != nil {
+		return []Tag{}, Data{}, fmt.Errorf("Parse() -> %s", err)
+	}
+
+	return tagList, data, nil
 }
 
 // NewSet generates a set that's retuned as pointer
-func NewSet(buff io.Reader) *Set {
+func NewSet(buff []byte) *Set {
 
-    return &Set{
+	return &Set{
 
-        Fields: make([]Tag, 0),
-        
-        Content: Data{
-            Type: nil,
-            Index: 0,
-            Value: "",
-            Inners: make([]Data, 0),
-        },
-        
-        Raw: buff,
-    }
+		Fields: make([]Tag, 0),
+
+		Content: Data{
+			Type:   nil,
+			Index:  0,
+			Value:  "",
+			Inners: make([]Data, 0),
+		},
+
+		Raw: buff,
+	}
 }
 
 // recurse recreates a recursive Data datastructure representation of the file itself. It's taking every tags, subtags and data to make them one recursive datastructure of Type Data
 func recurse(decoder *xml.Decoder, tagList []Tag) (Data, error) {
 
-    data := newData(0)
-    index := 0
+	data := newData(0)
+	index := 0
 
-    for {
-        tok, err := decoder.Token()
-        if err == io.EOF {
-            break
-        }
-        
-        switch tk := tok.(type) {
+	for {
+		tok, err := decoder.Token()
+		if err == io.EOF {
+			break
+		}
 
-            case xml.StartElement:
-                name := tk.Name.Local
+		switch tk := tok.(type) {
 
-                data.Inners = append(data.Inners, newData(index))
-                data.Inners[index].Type = getTag(tagList, name)
+		case xml.StartElement:
+			name := tk.Name.Local
 
-                data.Inners[index], err = recurse(decoder, tagList)
-                if err != nil {
-                    return Data{}, fmt.Errorf("recurse() -> %s", err)
-                }
+			data.Inners = append(data.Inners, newData(index))
+			data.Inners[index].Type = getTag(tagList, name)
 
-                index = index + 1
+			data.Inners[index], err = recurse(decoder, tagList)
+			if err != nil {
+				return Data{}, fmt.Errorf("recurse() -> %s", err)
+			}
 
-                fmt.Println("Start:", data, index)
+			index = index + 1
 
-            case xml.EndElement:
-                fmt.Println("Start:", data, index)
-                return data, nil
-            
-            case xml.CharData:
-                data.Value = string(tk)
-                fmt.Println("Start:", data, index)
-            
-            default:
-                return Data{}, fmt.Errorf("recurse() -> Unknown or unused Type encountered, got %T", tok)
-        }
-    }
+		case xml.EndElement:
+			return data, nil
 
-    return data, nil
+		case xml.CharData:
+			data.Value = string(tk)
+
+		default:
+			return Data{}, fmt.Errorf("recurse() -> Unknown or unused Type encountered, got %T", tok)
+		}
+	}
+
+	return data, nil
 }
 
 // newData return an empty Data Type
 func newData(index int) Data {
 
-    return Data{
-        Type: nil,
-        Index: index,
-        Value: "",
-        Inners: make([]Data, 0),
-    }
+	return Data{
+		Type:   nil,
+		Index:  index,
+		Value:  "",
+		Inners: make([]Data, 0),
+	}
 }
 
 // findTags find and return the exhaustive list of unique tags found in the xml file
 func findTags(decoder *xml.Decoder) []Tag {
 
-    tagList := make([]Tag, 0)
-    index := 0
+	tagList := make([]Tag, 0)
+	index := 0
 
-    for {
-        tok, err := decoder.Token()
-        if err == io.EOF {
-            break
-        }
-        
-        switch tk := tok.(type) {
+	for {
+		tok, err := decoder.Token()
+		if err == io.EOF {
+			break
+		}
 
-            case xml.StartElement:
+		switch tk := tok.(type) {
 
-                if !tagExist(tk.Name.Local, tagList) {
-                    
-                    tagList = append(tagList, Tag{
-                        Id: index,
-                        Name: tk.Name.Local,
-                    })
-                }
-        }
-    }
-    return tagList
+		case xml.StartElement:
+
+			if !tagExist(tk.Name.Local, tagList) {
+
+				tagList = append(tagList, Tag{
+					Id:   index,
+					Name: tk.Name.Local,
+				})
+			}
+		}
+	}
+	return tagList
 }
 
 // getTag return a single tag from a tagList with a given name as string
 func getTag(tagList []Tag, tok string) *Tag {
 
-    for index, tag := range tagList {
-    
-        if tok == tag.Name {
-            return &tagList[index]
-        }
-    }
-    
-    return nil
+	for index, tag := range tagList {
+
+		if tok == tag.Name {
+			return &tagList[index]
+		}
+	}
+
+	return nil
 }
 
 // tagExist checks if a tag exist in the whole list of tags
 func tagExist(word string, tagList []Tag) bool {
-    
-    for _, tag := range tagList {
-    
-        if tag.Name == word {
-            return true
-        }
-    }
-    
-    return false
+
+	for _, tag := range tagList {
+
+		if tag.Name == word {
+			return true
+		}
+	}
+
+	return false
 }
